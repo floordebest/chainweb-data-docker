338d6e2df6a0d151181ddf0484d890f231239a34
Floppie <45384699+floordebest@users.noreply.github.com>
2022-05-30 13:56:54 +0200

    Update gaps.sh

--- gaps.sh
+++ gaps.sh
@@ -6,6 +6,6 @@ fi
 
 date_timestamp=$(date '+%Y-%m-%d %H:%M:%S')
 echo -e "Fill was started at $date_timestamp" >> /tmp/fill_history.log
-/usr/local/bin/chainweb-data fill --service-host=172.15.0.1 --p2p-host=172.15.0.1 --service-port=31351 --p2p-port=31350 --dbuser=postgres --dbpass=postgres --dbname=postgres
+/usr/local/bin/chainweb-data fill --service-host=104.248.86.98 --p2p-host=104.248.86.98 --service-port=8080 --p2p-port=1917 --dbuser=postgres --dbpass=postgres --dbname=postgres
 date_timestamp=$(date '+%Y-%m-%d %H:%M:%S')
 echo -e "Fill was ended at $date_timestamp" >> /tmp/fill_history.log
